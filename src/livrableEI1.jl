15abb342d71db0bbcd8ca99acec553e3630d0faa
marrrryzebwjq <marie-2001-point@hotmail.fr>
2024-11-06 10:43:56 +0100

    màj ei1 et ei2

--- src/livrableEI1.jl
+++ src/livrableEI1.jl
@@ -12,9 +12,8 @@ function construct(C, A)
         x[j]=top/bottom
     end
     sort_ind=sortperm(x,rev=true)
-    x=x[sort_ind]
-    x0=zeros(n)
-    sum
+    x0=zeros(Int,n)
+    sum=0
     for j=1:n
         x0[sort_ind[j]]=1
         for i=1:m
@@ -25,192 +24,175 @@ function construct(C, A)
             end
         end
     end
+    sum=0
+    for j in eachindex(x0)
+        if(x0[j]!=0)
+            sum+=1
+        end
+    end
+    println("sum est ", sum)
     println("on prie")
     res=eco(C,x0)
     println("la somme nous donne:",res)
     k=2
     start=time()
-    kp=run(C,x0)
+    kp=copy(run(A,C,x0))
     tutilise = time()-start
     println(tutilise)
+    println(isValid(A,x0))
+    println(eco(C,x0))
     println("la somme est vraiment:",eco(C,kp))
 
 end
-function run(C,x)
+function run(A,C,x)
     tmp=eco(C,x)
-    kp=ckp(A,x)
-    tmp2=x
+    kp=copy(ckp(A,C,x))
+    tmp2=copy(x)
     while(eco(C,kp)>tmp)
         tmp=eco(C,kp)
-        tmp2=kp
-        kp=ckp(A,x)
+        tmp2=copy(kp)
+        kp=copy(ckp(A,C,x))
     end
     tmp=-1
-    kp=tmp2
+    kp=copy(tmp2)
     while(eco(C,kp)>tmp)
         tmp=eco(C,kp)
-        tmp2=kp
-        kp=bkp(A,kp)
+        tmp2=copy(kp)
+        kp=copy(bkp(A,C,kp))
     end
     tmp=-1
-    kp=tmp2
+    kp=copy(tmp2)
     while(eco(C,kp)>tmp)
         tmp=eco(C,kp)
-        tmp2=kp
-        kp=akp(A,kp)
+        tmp2=copy(kp)
+        kp=copy(akp(A,C,kp))
     end
-    kp=tmp2
+    kp=copy(tmp2)
     return kp
 end
+function run2(A,C,x0)
+    return akp(A,C,akp(A,C,bkp(A,C,akp(A,C,bkp(A,C,bkp(A,C,ckp(A,C,ckp(A,C,x0))))))))
+end
 function eco(C,x)
-    sum=dot(C,x)
+    sum=0
+    for i in eachindex(x)
+        sum+=C[i]*x[i]
+    end
     return sum
 end
+function eco2(C,base,i,j,k)
+    return C[k]-C[i]-C[j]+base
+end
+function eco3(C,base,i,j)
+    return C[i]-C[j]+base
+end
+function eco4(C,base,i)
+    return C[i]+base
+end
 
-function akp(A,x0)
-    liste=findall(x->x==0, x0)
+function akp(A,C,x0)
+    nb_zeros=count(a->a==0,x0)
+    zero=zeros(Int, nb_zeros)
     cp=copy(x0)
-    cp1=[]
     res=eco(C,x0)
     x_res=copy(x0)
-    for i in eachindex(liste)
-        cp1=copy(cp)
-        cp[liste[i]]=1
-        if(res<eco(C,cp))
-            if(isValid(A,cp))
-            res=eco(C,cp)
+    it=1
+    tmp=0.0
+    for i in eachindex(x0)
+        if(x0[i]==0)
+            zero[it]=i
+            it+=1
+        end
+    end
+    for i in eachindex(zero)
+        cp[zero[i]]=1
+        tmp=eco4(C,res,zero[i])
+        if(res<tmp)
+            if(isValid4(A,cp,zero[i]))
+            res=tmp
             x_res=copy(cp)
             end
         end
-        cp=copy(x0)
+        cp[zero[i]]=0
     end
     println("la valeur de akp est: ", res)
     return x_res
 end
 
-function bkp(A,x0)
+function bkp(A,C,x0)
     cp=copy(x0)
-    cp1=copy(x0)
-    m=length(x0)
+    nb_zeros=count(a->a==0,x0)
+    nb_ones=count(a->a==1,x0)
+    zero=zeros(Int, nb_zeros)
+    one=ones(Int, nb_ones)
     x_res=copy(x0)
     res=eco(C,x0)
-    for i=1:m
-        cp=copy(x0)
-        if(cp[i]==1)
-            cp[i]=0
-            for j=i+1:m
-                cp1=copy(cp)
-                if(j!=i && cp[j]==0)
-                    cp[j]=1
-                    if(res<eco(C,cp))
-                        if(isValid(A,cp))
-                        res=eco(C,cp)
-                        x_res=copy(cp)
-                        end
-                    end
-                    cp=copy(cp1)
-                end
-            end
+    tmp=0.0
+    it1=1
+    it2=1
+    for i in eachindex(x0)
+        if(x0[i]==1)
+            one[it1]=i
+            it1+=1
         else
-            cp[i]=1
-            if(res<eco(C,cp))
-                for j=i+1:m
-                    cp1=copy(cp)
-                    if(j!=i && cp[j]==1)
-                        cp[j]=0
-                        if(res<eco(C,cp))
-                            if(isValid(A,cp))
-                            res=eco(C,cp)
-                            x_res=copy(cp)
-                            end
-                        end
-                        cp=copy(cp1)
-                    end
-                end
-            end
+            zero[it2]=i 
+            it2+=1
         end
+    end
+    for i in eachindex(zero)
+        cp[zero[i]]=1
+            for k in eachindex(one)
+                cp[one[k]]=0
+                tmp=eco3(C,res,zero[i],one[k])
+                if(res<(tmp) && isValid3(A,cp,zero[i],one[k]))
+                    x_res=copy(cp)
+                    res=tmp
+                end
+                cp[one[k]]=1
+            end
+        cp[zero[i]]=0
     end
     println("valeur de bkp ", res)
     return x_res
 end
 
-function ckp(A,x0)
+function ckp(A,C,x0)
     cp=copy(x0)
     x_res=copy(x0)
     res=eco(C,x0)
-    m=length(x0)
-    zeros=findall(a->a==0,x0)
-    ones=findall(a->a==1,x0)
-    for i=1:m
-        cp=copy(x0)
+    nb_zeros=count(a->a==0,x0)
+    nb_ones=count(a->a==1,x0)
+    one=ones(Int,nb_ones)
+    zero=zeros(Int,nb_zeros)
+    it1=1
+    it2=1
+    tmp=0.0
+    for i in eachindex(x0)
         if(x0[i]==1)
-            cp[i]=0
-            for j=i+1:m
-                cp1=copy(cp)
-                if(j!=i)
-                    if(cp[j]==1)                    
-                            cp[j]=0
-                            for k=j+1:m
-                                cp2=copy(cp)
-                                 if(k!=j && k!=i && cp[k]==0)
-                                    cp[k]=1
-                                    if(res<eco(C,cp))
-                                        if(isValid(A,cp))
-                                            x_res=copy(cp)
-                                            res=eco(C,cp)
-                                        end
-                                   end
-                               end
-                            cp=copy(cp2)
-                        end
-                    else
-                        cp[j]=1
-                        if(res<eco(C,cp))
-                            for k=j+1:m
-                                cp2=copy(cp)
-                                if(k!=j && k!=i && cp[k]==1)
-                                    cp[k]=0
-                                    if(res<eco(C,cp))
-                                        if(isValid(A,cp))
-                                            x_res=copy(cp)
-                                            res=eco(C,cp)
-                                        end
-                                    end
-                                end
-                                cp=copy(cp2)
-                            end
-                        end
-                    end
-                end
-                cp=copy(cp1)
-            end
+            one[it1]=i
+            it1+=1
         else
-            cp[i]=1
-            if(res<eco(C,cp))
-                for j=i+1:m
-                    cp1=copy(cp)
-                    if(j!=i)
-                        if(cp[j]==1)
-                            cp[j]=0
-                            for k=j+1:m
-                                cp2=copy(cp)
-                                if(k!=j && k!=i && cp[k]==1)
-                                    cp[k]=0
-                                    if(res<eco(C,cp))
-                                        if(isValid(A,cp))
-                                            x_res=copy(cp)
-                                            res=eco(C,cp)
-                                        end
-                                    end
-                                end
-                                cp=copy(cp2)
-                            end
-                        end
-                    end
-                    cp=copy(cp1)
-                end
-            end
+            zero[it2]=i 
+            it2+=1
         end
+    end
+    for i in eachindex(one)
+        cp[one[i]]=0
+        for j=i+1:length(one)
+            cp[one[j]]=0
+            for k in eachindex(zero)
+                cp[zero[k]]=1
+                tmp=eco2(C,res,one[i],one[j],zero[k])
+                if(isValid2(A,cp,one[i],one[j],zero[k]) && res<tmp)
+                    x_res=copy(cp)
+                    res=tmp
+                    println("Ã©change ", one[i]," ", one[j], " ", zero[k])
+                end
+                cp[zero[k]]=0
+            end
+            cp[one[j]]=1
+        end
+        cp[one[i]]=1
     end
     println("la valeur de ckp est: ", res)
     return x_res
@@ -225,10 +207,11 @@ function max(x1,x2)
 end
 
 function isValid(A,x)
-    m,n = size(A)
+    m,n= size(A)
+    sum=0
     for i=1:m
         sum=0
-        for j = 1:n
+        for j=1:n
             if(x[j]==1 && A[i,j]==1)
                 sum+=1
                 if(sum>1)
@@ -240,5 +223,47 @@ function isValid(A,x)
     return true
 end
 
-function solution()
+function isValid2(A,x,i,j,k)
+    m,n = size(A)
+    sum=0
+    for a=1:m 
+        sum=0
+        if(A[a,i]==0 && A[a,j]==0 && A[a,k]==1)
+            sum=dot(A[a,:],x)
+            if(sum>1)
+                return false
+            end
+        end
+    end
+    return true
+end
+
+function isValid3(A,x,i,k)
+    m,n = size(A)
+    sum=0
+    for a=1:m 
+        sum=0
+        if(A[a,k]==0 && A[a,i]==1)
+            sum=dot(A[a,:],x)
+            if(sum>1)
+                return false
+            end
+        end
+    end
+    return true
+end
+
+function isValid4(A,x,i)
+    m,n = size(A)
+    sum=0
+    for a=1:m 
+        sum=0
+        if(A[a,i]==1)
+            sum=dot(A[a,:],x)
+            if(sum>1)
+                return false
+            end
+        end
+    end
+    return true
 end
--- src/livrableEI2.jl
+++ src/livrableEI2.jl
@@ -1,11 +1,16 @@
 using LinearAlgebra
 using Random
 include("livrableEI1.jl")
-function construct2(C, A)
+
+function construct2(C, A, alpha)
     m, n = size(A)
     eval = zeros(Float64, n)
+    cand=[]
     max=0
     min=0
+    for i=1:n
+        push!(cand,i)
+    end
     for j=1:n
         top=C[j]
         bottom=0
@@ -20,11 +25,6 @@ function construct2(C, A)
             min=eval[j]
         end
     end
-    alpha=rand((0.6,0.9))
-    cand=[]
-    for i=1:n
-        push!(cand,i)
-    end
     x0=zeros(n)
     while(!isempty(cand))
         evali=copy(eval[cand])
@@ -51,61 +51,46 @@ function construct2(C, A)
         cand=setdiff(cand,del)
         x0[e2]=1
     end
-    println("la valeur de la fonction économique est:", eco(C,x0))
     return x0
 end
 
-function buildRCL(eval,alpha,min,max)
-    alpha=rand((0.6,0.9))
-    m, n = size(A)
-    cand=[]
+function buildRCL(x,alpha,min,max)
+    limit=min+alpha*(max-min)
+    ind=1
+    RCL=[]
     for i=1:n
-        push!(cand,i)
-    end
-    x0=zeros(n)
-    while(!isempty(cand))
-        evali=copy(eval[cand])
-        min=minimum(evali)
-        max=maximum(evali)
-        limit=min+alpha*(max-min)
-        println(limit)
-        RCL=[]
-        for i in eachindex(evali)
-            if(evali[i]>=limit)
-                push!(RCL,i)
-            end
+        if(x[i]>=limit)
+            push!(RCL,ind)
         end
-        e=rand(collect(RCL))
-        e2=cand[e]
-        cand=filter!(a->a!=e, cand)
-        del=[]
-        println(cand)
-        for i in eachindex(cand)
-            for j in 1:m
-                if(A[j,cand[i]]+A[j,e2]>1)
-                    push!(del,cand[i])
-                end
-            end
-        end
-        cand=setdiff(cand,del)
-        x0[e2]=1
     end
+    return limit, RCL
 end
 
 function GRASP(C,A)
-    res=0
-    j=0
-    x_res=[]
-    while j<20
-        init=construct2(C,A)
-        improved=run(C,init)
-        tmp=eco(C,improved)
-        if(res<tmp)
-            res=tmp
-            x_res=copy(improved)
-        end
-        j+=1
+    alpha=rand((0.6,0.9))
+    s=[]
+    m=length(C)
+    x=zeros(m)
+    k=20
+    for i=1:k
+        x=copy(construct2(C,A,alpha))
+        x=copy(run(A,C,x))
+        push!(s,x)
     end
-    println("valeur finale: ", res)
+    val=zeros(k)
+    tmp=-1
+    ind=0
+    for i in eachindex(val)
+        val[i]=eco(C,s[i])
+        if(eco(C,val[i])>tmp)
+            tmp=eco(C,val[i])
+            ind=i
+        end
+    end
+    x_res=copy(s[ind])
+    println(eco(C,x_res))
     return x_res
+end
+
+function reactiveGRASP(C,A)
 end
